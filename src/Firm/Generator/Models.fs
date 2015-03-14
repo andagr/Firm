0087f47985e22814950c7d6c7bc3a8d94f092f3b
Andreas Ågren <bandagr@gmail.com>
2015-03-14 23:25:05 +0100

    Removed name in PostModel.ToString.

--- src/Firm/Generator/Models.fs
+++ src/Firm/Generator/Models.fs
@@ -11,7 +11,7 @@ type PostModel(title: string, date: DateTime, tags: IEnumerable<string>, documen
     override t.ToString() =
         let docLength = min (String.length document) 10
         let docBeginning = document.Substring(0, docLength) + "..."
-        sprintf "Name: %s, Title: %s, Date: %A, Tags: %A, Document: %s" name title date tags docBeginning
+        sprintf "Title: %s, Date: %A, Tags: %A, Document: %s" title date tags docBeginning
 
 type SinglePostModel(disqusShortname: string, post: PostModel) =
     member t.DisqusShortname = disqusShortname
