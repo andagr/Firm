4849687bf1d726e9577b3b985a64acf6f850af80
Andreas Ågren <bandagr@gmail.com>
2015-04-19 22:16:55 +0200

    Made firm cmd script a bit more user friendly

--- firm.cmd
+++ firm.cmd
@@ -1,44 +1,62 @@
 @echo off
+setlocal
 
-if "%1"=="help" goto help
+set root=%~dp0
 
-if "%1"=="init" goto init
-if not exist packages call :init
+if "%1"=="reset" goto :reset
+if "%1"=="init" goto :init
+if "%1"=="clean" goto :clean
+if "%1"=="build" goto :build
+if "%1"=="generate" goto :generate
+if "%1"=="preview" goto :preview
+goto :help
 
-if "%1"=="clean" goto clean
-if "%1"=="build" goto build
-if not exist bin call :build
-
-if "%1"=="generate" goto generate
-if not exist output call :generate
-
-if "%1"=="preview" goto preview
-
-
-
+:reset
+if exist packages (
+    echo Removing packages...
+    rd /q /s packages
+)
+if exist bin (
+    echo Removing bin...
+    rd /s /q bin
+)
+if exist output (
+    echo Removing output...
+    rd /s /q output
+)
+echo Done!
+goto :end
 
 :init
+echo Initializing packages...
 .paket\paket.exe restore
-goto end
+echo Done!
+goto :end
 
 :clean
 :build
+if not exist packages call :init
+echo Calling FAKE with target "%1"...
 packages\FAKE\tools\FAKE.exe fake.fsx %1
-goto end
+echo Done!
+goto :end
 
 :generate
-rem dummy for now
-mkdir output
-goto end
+if not exist bin call :build
+echo Generating site...
+bin\Generator.exe "%root%"
+goto :end
 
 :preview
-set root=%~dp0
+if not exist output call :generate
+echo Starting HTTP server to preview site...
 bin\Host.exe "%root%output\\"
-goto end
-
+echo Done!
+goto :end
 
 :help
 echo Available commands:
+echo   reset     Removes all downloaded and generated files/folders.
 echo   init      Installs the required paket dependencies.
 echo   clean     Cleans the build directory.
 echo   build     Builds the firm application.
@@ -46,3 +64,4 @@ echo   generate  Generates the web site.
 echo   preview   Starts a local http server for previewing the generated blog.
 
 :end
+endlocal
