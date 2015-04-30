0f9a0a9d1e3cbf16a49d2c8d991b72e3e9aa660a
Andreas Ågren <bandagr@gmail.com>
2015-04-30 20:38:19 +0200

    Revert "Added support for custom url root"
    This reverts commit 0b40d5f67c3c010f0b6c1445574002404bdd90fe.

--- src/Firm/Generator/Models.fs
+++ src/Firm/Generator/Models.fs
@@ -2,15 +2,6 @@
 
 open System
 
-type ModelHelper() =
-    static member LocalUrl(urlRoot: string, path: string) =
-        if urlRoot.EndsWith("/") && path.StartsWith("/") then
-            urlRoot.Remove(String.length urlRoot - 1) + path
-        else if urlRoot.EndsWith("/") || path.StartsWith("/") then
-            urlRoot + path
-        else
-            urlRoot + "/" + path
-
 type PostModel(name: string, title: string, date: DateTime, tags: string seq, document: string) =
     member t.Name = name
     member t.Title = title
@@ -26,21 +17,18 @@ type TagModel(name: string, count: int) =
     member t.Name = name
     member t.Count = count
 
-type SinglePostModel(urlRoot: string, disqusShortname: string, post: PostModel, allPosts: PostModel seq, tags: TagModel seq) =
-    member t.LocalUrl(path: string) = ModelHelper.LocalUrl(urlRoot, path)
+type SinglePostModel(disqusShortname: string, post: PostModel, allPosts: PostModel seq, tags: TagModel seq) =
     member t.DisqusShortname = disqusShortname
     member t.Post = post
     member t.AllPosts = allPosts
     member t.Tags = tags
 
-type AllPostsModel(urlRoot: string, disqusShortname: string, allPosts: PostModel seq, tags: TagModel seq) =
-    member t.LocalUrl(path: string) = ModelHelper.LocalUrl(urlRoot, path)
+type AllPostsModel(disqusShortname: string, allPosts: PostModel seq, tags: TagModel seq) =
     member t.DisqusShortname = disqusShortname
     member t.AllPosts = allPosts
     member t.Tags = tags
 
-type PageModel(urlRoot: string, disqusShortName: string, document: string, allPosts: PostModel seq, tags: TagModel seq) =
-    member t.LocalUrl(path: string) = ModelHelper.LocalUrl(urlRoot, path)
+type PageModel(disqusShortName: string, document: string, allPosts: PostModel seq, tags: TagModel seq) =
     member t.DisqusShortname = disqusShortName
     member t.Document = document
     member t.AllPosts = allPosts
