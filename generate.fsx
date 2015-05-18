fdb49d178ec960f53e54fce46c1b8c765e0e5db6
Andreas Ågren <bandagr@gmail.com>
2015-05-19 00:21:08 +0200

    Test function for modifying urls from local absolute to absolute

--- generate.fsx
+++ generate.fsx
@@ -3,6 +3,7 @@
 #r "bin/FSharp.Markdown.dll"
 #r "bin/FSharp.Literate.dll"
 
+open System.Collections.Generic
 open FSharp.Literate
 open FSharp.Markdown
 
@@ -14,9 +15,14 @@ Hello world in [F#](http://fsharp.net) looks like this:
 
     printfn "Hello world!"
 
+Hello world 2 in [F#](/fsharp/) looks like this:
+
+Test: [test][2]
+
 For more see [fsharp.org][1].
 
-  [1]: http://fsharp.org """
+  [1]: http://fsharp.org
+  [2]: /test """
 
 
 
@@ -44,3 +50,33 @@ let rec collectParLinks par = seq {
 /// Collect links in the entire document
 Seq.collect collectParLinks parsed.Paragraphs
 |> List.ofSeq
+
+
+let toAbsUrl (prefix : string) url =
+    match url with
+    | u when String.length u >= 2 && u.StartsWith("//") -> u
+    | u when u.StartsWith("/") -> prefix.TrimEnd('/') + u
+    | u -> u
+
+let withAbsUrls (doc : LiterateDocument) =
+    let rec fromSpan = function
+        | DirectLink(spans, (url, title)) -> DirectLink(spans, (toAbsUrl "http://localhost/" url, title))
+        | Matching.SpanNode(sni, ss) ->
+            let ss = List.map fromSpan ss
+            Matching.SpanNode(sni, ss)
+        | other -> other
+    let rec fromPar = function
+        | Matching.ParagraphNested(pni, nested) ->
+            let ps = List.map (List.map fromPar) nested
+            Matching.ParagraphNested(pni, ps)
+        | Matching.ParagraphSpans(si, ss) ->
+            let ss = List.map fromSpan ss
+            Matching.ParagraphSpans(si, ss)
+        | other -> other
+    let fromDefLinks (defLinks:IDictionary<string, string * string option>) =
+        defLinks
+        |> Seq.map (fun kvp -> kvp.Key, (toAbsUrl "http://localhost/" (fst kvp.Value), snd kvp.Value))
+        |> dict
+    doc.With(paragraphs = List.map fromPar doc.Paragraphs, definedLinks = fromDefLinks doc.DefinedLinks)
+
+Literate.WriteHtml(withAbsUrls parsed)
