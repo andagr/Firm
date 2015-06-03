37b4eccc8a93749369e9c04e355a791fd4f00e7a
Andreas Ågren <bandagr@gmail.com>
2015-06-03 21:25:16 +0200

    Removed unused option for self-host

--- src/Firm/Host/Program.fs
+++ src/Firm/Host/Program.fs
@@ -8,23 +8,19 @@ open Microsoft.Owin.StaticFiles
 open Owin
 
 let parseArgs = function
-    | [|e; b|] -> (e, Some b)
-    | [|e|] -> (e, None)
-    | _ -> (Directory.GetCurrentDirectory(), None)
+    | [|e|] -> e
+    | _ -> Directory.GetCurrentDirectory()
 
-let configure root (baseUri : string option) (app : IAppBuilder) =
-    let options =
-        match baseUri with
-        | Some b -> FileServerOptions(EnableDirectoryBrowsing = true, FileSystem = PhysicalFileSystem(root), RequestPath = PathString.FromUriComponent(b))
-        | None -> FileServerOptions(FileSystem = PhysicalFileSystem(root), RequestPath = PathString.Empty)
+let configure root (app: IAppBuilder) =
+    let options = FileServerOptions(FileSystem = PhysicalFileSystem(root), RequestPath = PathString.Empty)
     app.UseFileServer(options) |> ignore
 
 [<EntryPoint>]
 let main args = 
-    let (root, baseUri) = parseArgs args
+    let root = parseArgs args
     printfn "Starting http server on path: %s" root
-    use s = WebApp.Start("http://localhost:8080", configure root baseUri)
-    let url = "http://localhost:8080" + (baseUri |> (function | Some b -> b | None -> ""))
+    use s = WebApp.Start("http://localhost:8080", configure root)
+    let url = "http://localhost:8080"
     printfn "Listening at %s" url
     printfn "Press enter to exit..."
     Process.Start(url) |> ignore
